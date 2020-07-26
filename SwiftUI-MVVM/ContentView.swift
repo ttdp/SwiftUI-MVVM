879019b20917fcb3b8ecc9e0f3672d700c8e71a9
ttdp <tiantong.dev@gmail.com>
2020-07-26 08:28:50 +0800

    Initial commit

---
+++ SwiftUI-MVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-MVVM
+//
+//  Created by Tian Tong on 7/26/20.
+//  Copyright © 2020 TTDP. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
